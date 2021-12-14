98b989a6b9ccc92c5cc921db7fdd31c48f11b54b
Samuelisch <samuelchanwk@hotmail.com>
2021-12-14 20:27:32 +0800

    bundled instances of images into flexbox in home component

--- src/components/home/ImgGrid.js
+++ src/components/home/ImgGrid.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import puzzleImages from '../images/puzzleImages'
+import ImgInstance from './ImgInstance'
+
+const StyledGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0;
+  border: 3px solid green; ${/* temp */''}
+`
 
 const ImgGrid = () => {
+  const puzzles = puzzleImages.map(puzzle => (
+    <ImgInstance 
+      key={puzzle.id}
+      puzzle={puzzle}
+    />
+  ))
+
   return (
-    <h1>Hello ImageGrid</h1>
+    <StyledGrid>
+      {puzzles}
+    </StyledGrid>
   )
 }
 
