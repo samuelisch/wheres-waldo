e4de1be92c9bc772e1e4e1593d3025b9bbe44b39
Samuelisch <samuelchanwk@hotmail.com>
2021-12-14 20:26:15 +0800

    added styling for header, title for app

--- src/components/assets/Header.js
+++ src/components/assets/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+  padding: 10px;
+  background: rgb(0, 150, 255);
+
+  h1 {
+    font-size: 2rem;
+  }
+`
 
 const Header = () => {
   return (
-    <h1>Hello Header</h1>
+    <StyledHeader>
+      <h1 id="title">Find Waldo</h1>
+    </StyledHeader>
   )
 }
 
