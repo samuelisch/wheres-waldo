ceb897de41efaa458f74af0d017757945fec2443
Samuelisch <samuelchanwk@hotmail.com>
2021-12-14 20:27:07 +0800

    initialised single instance component of puzzle images for homepage

--- src/components/home/ImgInstance.js
+++ src/components/home/ImgInstance.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const ImgInstance = () => {
+const StyledInstance = styled.div`
+  margin: 0 auto;
+  width: 90%;
+  max-width: 500px;
+  border: 1px solid red; ${/* temp */''}
+`
+
+const StyledImg = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 500px;
+  
+`
+
+const ImgInstance = ({ puzzle }) => {
   return (
-    <h1>Hello ImgInstance</h1>
+    <Link to={`/game/${puzzle.link}`}>
+      <StyledInstance>
+        <StyledImg src={puzzle.imgSrc.default} />
+        <p>id: {puzzle.id}</p>
+        <p>name: {puzzle.name}</p>
+      </StyledInstance>
+    </Link>
   )
 }
 
