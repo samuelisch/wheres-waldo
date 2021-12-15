493e6ced85c7da1b04316a58f4051b13deb93a9a
Samuelisch <samuelchanwk@hotmail.com>
2021-12-15 10:31:19 +0800

    added styling to header, absolute positioning on certain width for character images, set dynamic page title for application, depending on which page the title is at, mapped characterImages passed from Page component

--- src/components/assets/Header.js
+++ src/components/assets/Header.js
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import CharImage from './CharImage'
 
 const StyledHeader = styled.div`
   padding: 10px;
   background: rgb(0, 150, 255);
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  width: 100%;
 
   h1 {
     font-size: 2rem;
+    padding-right: 50px;
   }
 `
 
-const Header = () => {
+const StyledCharacters = styled.div`
+  display: flex;
+  padding-right: 10px;
+
+  @media (min-width: 600px) {
+    padding: 0;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`
+
+const Header = ({ pageImage, charImages }) => {
+  const [pageTitle, setPageTitle] = useState('Find Waldo')
+
+  useEffect(() => {
+    if (pageImage) {
+      setPageTitle(pageImage)
+    }
+  }, [pageImage])
+
   return (
     <StyledHeader>
-      <h1 id="title">Find Waldo</h1>
+      <h1 id="title">{pageTitle}</h1>
+      {charImages &&
+        <StyledCharacters>
+          {charImages.map(img => (
+            <CharImage
+              key={img.id}
+              image={img}
+            />
+          ))}
+        </StyledCharacters>
+      }
     </StyledHeader>
   )
 }
