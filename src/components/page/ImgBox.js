1b60b9d9c391535c2eb177e95bfa268d62995867
Samuelisch <samuelchanwk@hotmail.com>
2021-12-15 10:32:33 +0800

    added some styling to Puzzle image loaded into application

--- src/components/page/ImgBox.js
+++ src/components/page/ImgBox.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const StyledContainer = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  max-width: 1440px;
+  margin: 0 auto;
+`
+
 const StyledImg = styled.img`
   width: 100%;
 `
 
 const ImgBox = ({ image }) => {
   return (
-    <>
+    <StyledContainer>
       <StyledImg src={image.imgSrc.default} />
-    </>
+    </StyledContainer>
   )
 }
 
