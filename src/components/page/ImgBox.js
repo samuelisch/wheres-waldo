c39ffa8a7c602f1440abc24dba2ba15194ba1630
Samuelisch <samuelchanwk@hotmail.com>
2021-12-16 22:23:38 +0800

    added states for image size on window resize, and coordinates of each click. initialised event handlers for clicks, and converts click coordinates in  a resized window to relate to coordinates click in a full sized window, where image is set at 1440px.

--- src/components/page/ImgBox.js
+++ src/components/page/ImgBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const StyledContainer = styled.div`
@@ -13,10 +13,56 @@ const StyledImg = styled.img`
 `
 
 const ImgBox = ({ image }) => {
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  })
+
+  const [click, setClick] = useState({
+    x: undefined,
+    y: undefined,
+  })
+
+  const showCoords = (e) => {
+    const puzzle = e.target
+    console.log('size: ', puzzle.width, puzzle.height)
+    console.log('coords: ', e.pageX, e.pageY)
+    console.log('top: ', puzzle.offsetTop)
+    console.log('left: ', puzzle.offsetLeft)
+    const relativeX = e.pageX - puzzle.offsetLeft
+    const relativeY = e.pageY - puzzle.offsetTop
+    console.log('relative coords: ', relativeX, relativeY)
+    setSize({
+      width: puzzle.width,
+      height: puzzle.height
+    })
+    setClick({
+      x: relativeX,
+      y: relativeY
+    })
+  }
+
+  const showComparison = () => {
+    console.log(`size of window: ${size.width}, ${size.height}`)
+    const widthRatio = 1440 / size.width
+    const heightRatio = 924 / size.height
+    console.log(`ratio of diff is ${widthRatio}, ${heightRatio}`)
+    console.log(`click area is ${click.x}, ${click.y}`)
+    console.log(`relative click is ${Math.round(click.x * widthRatio)}, ${Math.round(click.y * heightRatio)}`)
+  }
+
   return (
-    <StyledContainer>
-      <StyledImg src={image.imgSrc.default} />
-    </StyledContainer>
+    <>
+      <StyledContainer>
+        <StyledImg  
+          className="puzzleImage" 
+          src={image.imgSrc.default} 
+          alt={image.name} 
+          onClick={showCoords}  
+        />
+      </StyledContainer>
+      <button type="button" onClick={showComparison}>Show comparison</button>
+    </>
   )
 }
 
