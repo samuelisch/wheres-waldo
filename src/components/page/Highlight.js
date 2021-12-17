dacf359631da1545e9139aefbda8e38c98f31c96
Samuelisch <samuelchanwk@hotmail.com>
2021-12-17 22:48:10 +0800

    added absolute positioning for circle selection

---
+++ src/components/page/Highlight.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect} from 'react'
+import styled from 'styled-components'
+
+const StyledSelect = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 2px solid #121212;
+  border-radius: 100%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+`
+
+const Highlight = ({ hidden, position }) => {
+  const [boxPosition, setBoxPosition] = useState(position)
+
+  useEffect(() => {
+    setBoxPosition(position)
+  }, [position])
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <StyledSelect 
+      className={"cursor " + (hidden ? 'hidden' : ' ')} 
+      style={{
+        left: `${boxPosition.x}px`,
+        top: `${boxPosition.y}px`,
+      }} 
+    />
+  )
+}
+
+export default Highlight
