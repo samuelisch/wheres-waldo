a6507524b3548ff74d459f7863e69690640a63ce
Samuelisch <samuelchanwk@hotmail.com>
2021-12-18 10:14:12 +0800

    imported selection box to component, added styling changes to accomodate for larger screens, styled the parents div element bulding the smaller components together

--- src/components/page/Highlight.js
+++ src/components/page/Highlight.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect} from 'react'
 import styled from 'styled-components'
+import SelectionBox from './SelectionBox'
+
+const StyledContainer = styled.div`
+  position: absolute;
+  display: flex;
+`
 
 const StyledSelect = styled.div`
-  width: 40px;
-  height: 40px;
+  width: 30px;
+  height: 30px;
   border: 2px solid #121212;
   border-radius: 50%;
-  position: absolute;
   transform: translate(-50%, -50%);
   pointer-events: none;
+
+  @media (min-width: 1024px) {
+    width: 50px;
+    height: 50px;
+  }
 `
 
 const Highlight = ({ hidden, position }) => {
@@ -19,15 +29,16 @@ const Highlight = ({ hidden, position }) => {
   }, [position])
 
   return (
-    <div>
-      <StyledSelect 
-        className={"cursor " + (hidden ? 'hidden' : ' ')} 
-        style={{
-          left: `${boxPosition.x}px`,
-          top: `${boxPosition.y}px`,
-        }} 
-      />
-    </div>
+    <StyledContainer 
+      className={(hidden ? 'hidden' : ' ')}
+      style={{
+        left: `${boxPosition.x}px`,
+        top: `${boxPosition.y}px`,
+      }}
+    >
+      <StyledSelect />
+      <SelectionBox />
+    </StyledContainer>
   )
 }
 
