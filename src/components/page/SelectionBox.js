8d3b0e362834638e14b96b7326e7cb6eee579c2f
Samuelisch <samuelchanwk@hotmail.com>
2021-12-20 17:18:45 +0800

    added parameter and attribute for click event

--- src/components/page/SelectionBox.js
+++ src/components/page/SelectionBox.js
@@ -2,10 +2,11 @@ import React from 'react'
 import characterImages from '../images/characterImages'
 import SelectionTab from './SelectionTab'
 
-const SelectionBox = () => {
+const SelectionBox = ({ selectionEvent }) => {
   const toFind = characterImages.filter(character => !character.found)
   const tab = toFind.map(character => (
     <SelectionTab
+      selectionEvent={selectionEvent}
       key={character.id} 
       name={character.name}
     />
