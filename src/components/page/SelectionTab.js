d48dcd7391d8f75a3d68e349e87d14e24f216d98
Samuelisch <samuelchanwk@hotmail.com>
2021-12-20 17:17:26 +0800

    added event handler for click on tab

--- src/components/page/SelectionTab.js
+++ src/components/page/SelectionTab.js
@@ -6,7 +6,9 @@ const StyledTab = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 0 5px;
-  background: white;
+  background: rgba(245, 245, 245, 0.8);
+  -webkit-user-select: none; /* Chrome/Safari */ 
+  -moz-user-select: none; /* Firefox */
 
   &:first-child {
     border-radius: 5px 5px 0 0;
@@ -18,16 +20,18 @@ const StyledTab = styled.div`
 
   &:hover {
     background: rgb(230, 230, 230);
+    cursor: default;
   }
 
   @media (min-width: 1024px) {
     padding: 5px;
+    font-size: 1.3rem;
   }
 `
 
-const SelectionTab = ({ name }) => {
+const SelectionTab = ({ name, selectionEvent }) => {
   return (
-    <StyledTab>
+    <StyledTab onClick={() => selectionEvent(name)}>
       <p>{name}</p>
     </StyledTab>
   )
