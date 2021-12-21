c2bae36c2c1abec3bdb7c4261250cca992c57997
Samuelisch <samuelchanwk@hotmail.com>
2021-12-21 09:36:47 +0800

    minor styling changes, importing timer component

--- src/components/assets/Header.js
+++ src/components/assets/Header.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import CharImage from './CharImage'
+import Timer from './Timer'
 
 const StyledHeader = styled.div`
   background: rgb(0, 150, 255);
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   position: fixed;
   width: 100%;
   height: 75px;
@@ -15,6 +16,15 @@ const StyledHeader = styled.div`
     font-size: 2rem;
     margin: auto 0;
     padding: 0 50px 0 10px;
+    display: none;
+  }
+
+  @media (min-width: 750px) {
+    justify-content: space-between;
+
+    h1 {
+      display: block;
+    }
   }
 `
 
@@ -22,7 +32,7 @@ const StyledCharacters = styled.div`
   display: flex;
   padding-right: 10px;
 
-  @media (min-width: 600px) {
+  @media (min-width: 750px) {
     padding: 0;
     position: absolute;
     left: 50%;
@@ -34,7 +44,7 @@ const HeaderSpace = styled.div`
   height: 75px;
 `
 
-const Header = ({ pageImage, charImages }) => {
+const Header = ({ pageImage, charImages, timer }) => {
   const [pageTitle, setPageTitle] = useState('Find Waldo')
 
   useEffect(() => {
@@ -48,14 +58,17 @@ const Header = ({ pageImage, charImages }) => {
       <StyledHeader>
         <h1 id="title">{pageTitle}</h1>
         {charImages &&
-          <StyledCharacters>
-            {charImages.map(img => (
-              <CharImage
-                key={img.id}
-                image={img}
-              />
-            ))}
-          </StyledCharacters>
+          <>
+            <Timer timer={timer} />
+            <StyledCharacters>
+              {charImages.map(img => (
+                <CharImage
+                  key={img.id}
+                  image={img}
+                />
+              ))}
+            </StyledCharacters>
+          </>
         }
       </StyledHeader>
       <HeaderSpace>
