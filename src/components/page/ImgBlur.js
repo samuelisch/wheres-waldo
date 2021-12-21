7e2dd769cc0336e8e4e052d08d144c88822f0aa3
Samuelisch <samuelchanwk@hotmail.com>
2021-12-21 16:43:09 +0800

    separated components for start and end screens into their own components

--- src/components/page/ImgBlur.js
+++ src/components/page/ImgBlur.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import StartScreen from "./StartScreen"
+import EndScreen from "./EndScreen"
 
 const StyledOuterContainer = styled.div`
   position: absolute;
@@ -13,41 +15,17 @@ const StyledOuterContainer = styled.div`
   justify-content: center;
 `
 
-const StyledInnerContainer = styled.div`
-  margin-top: 15%;
-  font-size: 1.7rem;
-  text-align: center;
-
-  p {
-    font-weight: bold;
-    font-family: Courier;
-  }
-
-  button {
-    background: rgb(250, 80, 80);
-    border: 2px solid #B2560D;
-    border-radius: 5px;
-    padding: 5px 10px;
-    margin-top: 10px;
-    color: white;
-
-    &:hover {
-      background: rgb(250, 120, 120);
-    }
-
-    &:active {
-      background: rgb(250, 150, 150);
-    }
-  }
-`
-
 const ImgBlur = ({ gameEnd, btnClickHandler, timer }) => {
   return(
       <StyledOuterContainer>
-        <StyledInnerContainer>
-          <p>Find Waldo, Odlaw and Whitebeard!</p>
-          <button onClick={btnClickHandler}>Start game</button>
-        </StyledInnerContainer>
+        <>
+          {!gameEnd &&
+            <StartScreen btnClickHandler={btnClickHandler} />
+          }
+          {gameEnd &&
+            <EndScreen timer={timer} />
+          }
+        </>
       </StyledOuterContainer>
   )
 }
