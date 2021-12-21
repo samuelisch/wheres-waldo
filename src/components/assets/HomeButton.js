795cc81a80e95c93ffd981c3260af814c22e149a
Samuelisch <samuelchanwk@hotmail.com>
2021-12-21 19:32:53 +0800

    added Home button component to each game's pages

---
+++ src/components/assets/HomeButton.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+
+  button {
+    background: rgb(250, 80, 80);
+    border: 2px solid #B2560D;
+    border-radius: 5px;
+    padding: 5px 10px;
+    color: rgb(250,250,250);
+
+    &:hover {
+      background: rgb(250, 120, 120);
+    }
+  }
+`
+
+const HomeButton = () => {
+  return (
+    <StyledContainer>
+      <Link to="/"><button>Return to Main</button></Link>
+    </StyledContainer>
+  )
+}
+
+export default HomeButton
