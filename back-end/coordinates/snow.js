64a8e3ec5854e0d86ca16211e083a8b30aa6ae2b
Samuelisch <samuelchanwk@hotmail.com>
2021-12-22 22:16:21 +0800

    reverted back

--- back-end/coordinates/snow.js
+++
@@ -1,7 +0,0 @@
-const characters = {
-  'waldo': [1231, 675],
-  'odlaw' : [458, 593],
-  'whitebeard': [101, 701]
-}
-
-module.exports = characters
--- back-end/coordinates/space.js
+++
@@ -1,7 +0,0 @@
-const characters = {
-  'waldo': [584, 575],
-  'odlaw' : [103, 636],
-  'whitebeard': [1125, 531]
-}
-
-module.exports = characters
