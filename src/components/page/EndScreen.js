100c8210d8e31e41de72d0dd1376e1450aa96416
Samuelisch <samuelchanwk@hotmail.com>
2021-12-22 11:13:13 +0800

    added button to view leaderboards

--- src/components/page/EndScreen.js
+++ src/components/page/EndScreen.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 import { formatTime } from "../assets/utils"
 import Button from "../assets/Button"
@@ -9,12 +10,20 @@ const StyledContainer = styled.div`
   text-align: center;
 `
 
+const StyledButtons = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
 const EndScreen = ({ timer }) => {
   return (
     <StyledContainer>
       <p>You found all characters in {formatTime(timer)}!</p>
-      <p>You need to hit below {} to be on the leaderboards</p>
-      <Button btnClickHandler={() => window.location.reload()} text="Try again" />
+      <p>You are ranked #{} on the leaderboards</p>
+      <StyledButtons>
+        <Button btnClickHandler={() => window.location.reload()} text="Try again" />
+        <Link to="/"><Button text="View leaderboards" /></Link>
+      </StyledButtons>
     </StyledContainer>
   )
 }
