ebef2f6356b92dd94eaaa557db3b787cbcb72397
Samuelisch <samuelchanwk@hotmail.com>
2021-12-22 21:15:13 +0800

    added button link to leaderboards, in home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../assets/Header'
 import ImgGrid from './ImgGrid'
+import Button from '../assets/Button'
 import LoadingScreen from '../assets/LoadingScreen'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  text-align: center;
+
+  button {
+    padding: 20px;
+    font-size: 1.7rem;
+  }
+`
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,6 +34,9 @@ const Home = () => {
         <ImgGrid />
       </>
     }
+    <StyledDiv>
+      <Link to="leaderboards"><Button text="View leaderboards" /></Link>
+    </StyledDiv>
     </>
   )
 }
