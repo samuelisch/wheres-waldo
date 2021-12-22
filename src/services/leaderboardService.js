fa8c43d7bafd0ce5af3f58794a177e2d592dd522
Samuelisch <samuelchanwk@hotmail.com>
2021-12-22 17:33:49 +0800

    change in parameters for services for leaderboard

--- src/services/leaderboardService.js
+++ src/services/leaderboardService.js
@@ -1,32 +1,26 @@
 //service for leaderboards component
 import axios from "axios"
-const url = 'http://localhost:3001/leaderboards'
+const url = 'http://localhost:3001/api/leaderboards'
 
 const getAll = () => {
   const request = axios.get(url)
   return request.then(response => response.data)
 }
 
-const create = newObject => {
-  const request = axios.post(url, newObject)
+const create = (link, newObject) => {
+  const request = axios.post(`${url}/${link}`, newObject)
   return request.then(response => response.data)
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${url}/${id}`, newObject)
+const check = (link, timing) => {
+  const request = axios.put(`${url}/${link}`, timing)
   return request.then(response => response.data)
 }
 
-const remove = (id) => {
-  const request = axios.delete(`${url}/${id}`)
-  return request.then(response => response.data)
-}
-
-const services = {
+const leaderboardServices = {
   getAll, 
   create, 
-  update,
-  remove
+  check
 }
 
-export default services
+export default leaderboardServices
