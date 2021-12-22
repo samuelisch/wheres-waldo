a6ba59426d30a7c69b9016d73343312f56fe06d1
Samuelisch <samuelchanwk@hotmail.com>
2021-12-22 08:54:18 +0800

    added hover behaviour on image elements

--- src/components/home/ImgInstance.js
+++ src/components/home/ImgInstance.js
@@ -6,14 +6,21 @@ const StyledInstance = styled.div`
   margin: 0 auto;
   width: 90%;
   max-width: 500px;
-  border: 1px solid red; ${/* temp */''}
+  color: black;
+  margin-top: 20px;
+  box-shadow: 0 0 1px 1px rgb(150, 150, 150);
+  -webkit-transition-property: all;
+  -webkit-transition-duration: 0.3s;
+  -webkit-transition-timing-function: ease;
+
+  &:hover {
+    transform: scale(1.05)
+  }
 `
 
 const StyledImg = styled.img`
   width: 100%;
   height: 100%;
-  max-width: 500px;
-  
 `
 
 const ImgInstance = ({ puzzle }) => {
@@ -21,8 +28,6 @@ const ImgInstance = ({ puzzle }) => {
     <Link to={`/game/${puzzle.link}`}>
       <StyledInstance>
         <StyledImg src={puzzle.imgSrc.default} />
-        <p>id: {puzzle.id}</p>
-        <p>name: {puzzle.name}</p>
       </StyledInstance>
     </Link>
   )
